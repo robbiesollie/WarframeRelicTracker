84f26b23133d2c1756f3aa787fa5df5753ab7dd8
Robbie <robbiesollie@gmail.com>
2021-05-27 15:10:30 -0700

    completed crafting recipes and added some test data

---
+++ testData.sql
@@ -0,0 +1,36 @@
+source dbBuilder.sql;
+
+INSERT INTO Prime (Name, Quantity)
+	VALUES
+		('Burston', 0),
+		('Tipedo', 0),
+		('Gram', 1),
+		('Pyrana', 0);
+
+INSERT INTO Component (Name, Prime, Owned, Required)
+	VALUES
+		('Burston Prime Barrel', 'Burston', 1, 1),
+		('Burston Prime Receiver', 'Burston', 1, 1),
+		('Burston Prime Stock', 'Burston', 1, 1),
+		('Tipedo Prime Ornament', 'Tipedo', 1, 2),
+		('Tipedo Prime Handle', 'Tipedo', 1, 1),
+		('Gram Prime Blade', 'Gram', 2, 1),
+		('Gram Prime Handle', 'Gram', 0, 1),
+		('Pyrana Prime Barrel', 'Pyrana', 1, 1),
+		('Pyrana Prime Receiver', 'Pyrana', 0, 1);
+
+SELECT p.Name as Prime, c.Name as Component from Component c INNER JOIN Prime p ON c.Prime = p.Name;
+
+SELECT 'Craftable';
+
+SELECT p.Name as Prime FROM Prime p WHERE 
+	(SELECT COUNT(*) FROM Component c WHERE c.Prime = p.Name) <=
+	(SELECT COUNT(*) FROM Component c WHERE c.Prime = p.Name AND c.Owned >= c.Required);
+
+UPDATE Component SET Owned = Owned + 1 WHERE Name = "Tipedo Prime Ornament";
+
+SELECT 'Craftable';
+
+SELECT p.Name as Prime FROM Prime p WHERE 
+	(SELECT COUNT(*) FROM Component c WHERE c.Prime = p.Name) <=
+	(SELECT COUNT(*) FROM Component c WHERE c.Prime = p.Name AND c.Owned >= c.Required);
